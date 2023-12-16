34ad975457df0c75853269fcaa048a8ff025a275
Kakoi <your.email@example.com>
2023-12-16 19:35:29 +0700

    Add assigning device id

--- main/main.c
+++ main/main.c
@@ -13,8 +13,7 @@
 #include "driver/gpio.h"
 
 #define EXAMPLE_ADC1_CHAN0 ADC_CHANNEL_6
-#define DEVICE_ID 2
-#define HOST "http://192.168.1.8:8888"
+#define HOST "http://192.168.58.79:8888"
 #define MAX_HTTP_OUTPUT_BUFFER 256
 #define MAX_POST_SIZE 4096
 #define TEMP_SIZE 10
@@ -25,6 +24,9 @@
 #define LED_PIN GPIO_NUM_2
 #define LED_DELAY 200
 
+int device_id = 1;
+
+static const char *TAG_ASSiGN = "DEVICE_ASSIGN";
 static const char *TAG_HTTP_POST = "HTTP_POST";
 static const char *TAG_HTTP_GET = "HTTP_GET";
 static const char *TAG_ADC = "ONE_SHOT_ADC";
@@ -46,24 +48,35 @@ void oneshot_adc_read(int *value);
 void http_post_data(void *pvParameters);
 void http_get_data(void *pvParameters);
 void adc_oneshot_write(void *pvParameters);
+void cleanup_and_exit(esp_http_client_handle_t client, char *url, char *output_buffer);
 void led_toggle();
+int devide_id_assign();
 
-void cleanup_and_exit(esp_http_client_handle_t client, char *url, char *output_buffer)
+void app_main(void)
 {
-    if (client)
-    {
-        esp_http_client_cleanup(client);
-    }
-    if (url)
-    {
-        vPortFree(url);
-    }
-    if (output_buffer)
-    {
-        vPortFree(output_buffer);
-    }
-}
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    ESP_ERROR_CHECK(example_connect());
 
+    // Configure GPIO for LED
+    gpio_config_t io_conf = {
+        .pin_bit_mask = (1ULL << LED_PIN),
+        .mode = GPIO_MODE_OUTPUT,
+    };
+    gpio_config(&io_conf);
+
+    while (devide_id_assign())
+    {
+    }
+
+    xTaskCreatePinnedToCore(http_get_data, "Task0", 8192, NULL, 0, &task_http_get_data, 1);
+    xTaskCreatePinnedToCore(http_post_data, "Task1", 8192, NULL, 1, &task_http_post_data, 1);
+    xTaskCreatePinnedToCore(adc_oneshot_write, "Task2", 8192, NULL, 1, &task_adc_oneshot_write, 0);
+
+    vTaskSuspend(task_http_post_data);
+    vTaskSuspend(task_adc_oneshot_write);
+}
 static esp_err_t _http_event_handler(esp_http_client_event_t *evt)
 {
     switch (evt->event_id)
@@ -96,57 +109,25 @@ static esp_err_t _http_event_handler(esp_http_client_event_t *evt)
     return ESP_OK;
 }
 
-int http_post_json_handle(char *post_data)
-{
-    esp_http_client_handle_t client = esp_http_client_init(&(esp_http_client_config_t){
-        .url = "http://192.168.1.8:8888/esp",
-        .event_handler = _http_event_handler,
-        .method = HTTP_METHOD_POST,
-    });
-
-    if (!client)
-    {
-        ESP_LOGE(TAG_HTTP_POST, "Failed to initialize HTTP client");
-        return 1;
-    }
-
-    esp_http_client_set_header(client, "Content-Type", "application/json");
-    esp_http_client_set_post_field(client, post_data, strlen(post_data));
-
-    esp_err_t err = esp_http_client_perform(client);
-    if (err == ESP_OK)
-    {
-        ESP_LOGI(TAG_HTTP_POST, "HTTP POST Status = %d, content_length = %lld",
-                 esp_http_client_get_status_code(client),
-                 esp_http_client_get_content_length(client));
-    }
-    else
-    {
-        ESP_LOGE(TAG_HTTP_POST, "HTTP POST request failed: %s", esp_err_to_name(err));
-    }
-
-    cleanup_and_exit(client, NULL, NULL);
-    return (err == ESP_OK) ? 0 : 1;
-}
-
-void http_get_handle()
+int devide_id_assign()
 {
     char *url = (char *)pvPortMalloc(40);
+    bool is_existed = 1;
     if (!url)
     {
-        ESP_LOGE(TAG_HTTP_GET, "Failed to allocate memory for URL");
-        return;
+        ESP_LOGE(TAG_ASSiGN, "Failed to allocate memory for URL");
+        return 1;
     }
 
     char *output_buffer = (char *)pvPortMalloc(MAX_HTTP_OUTPUT_BUFFER + 1);
     if (!output_buffer)
     {
-        ESP_LOGE(TAG_HTTP_GET, "Failed to allocate memory for output buffer");
+        ESP_LOGE(TAG_ASSiGN, "Failed to allocate memory for output buffer");
         vPortFree(url);
-        return;
+        return 1;
     }
 
-    snprintf(url, 40, "%s/device?id=%d", HOST, DEVICE_ID);
+    snprintf(url, 40, "%s/assign?id=%d", HOST, device_id);
 
     esp_http_client_config_t config = {
         .url = url,
@@ -156,33 +137,45 @@ void http_get_handle()
 
     if (!client)
     {
-        ESP_LOGE(TAG_HTTP_GET, "Failed to initialize HTTP client");
-        cleanup_and_exit(NULL, url, output_buffer);
-        return;
+        ESP_LOGE(TAG_ASSiGN, "Failed to initialize HTTP client");
+        return 1;
     }
 
     esp_err_t err = esp_http_client_open(client, 0);
     if (err != ESP_OK)
     {
-        ESP_LOGE(TAG_HTTP_GET, "Failed to open HTTP connection: %s", esp_err_to_name(err));
+        ESP_LOGE(TAG_ASSiGN, "Failed to open HTTP connection: %s", esp_err_to_name(err));
     }
     else
     {
         int content_length = esp_http_client_fetch_headers(client);
         if (content_length < 0)
         {
-            ESP_LOGE(TAG_HTTP_GET, "HTTP client fetch headers failed");
+            ESP_LOGE(TAG_ASSiGN, "HTTP client fetch headers failed");
         }
         else
         {
             int data_read = esp_http_client_read_response(client, output_buffer, MAX_HTTP_OUTPUT_BUFFER);
             if (data_read >= 0)
             {
-                ESP_LOGI(TAG_HTTP_GET, "HTTP GET Status = %d, content_length = %" PRId64,
+                ESP_LOGI(TAG_ASSiGN, "HTTP GET Status = %d, content_length = %" PRId64,
                          esp_http_client_get_status_code(client),
                          esp_http_client_get_content_length(client));
-
-                logs = output_buffer[23] - '0';
+                is_existed = output_buffer[18] - '0';
+                if (is_existed)
+                {
+                    ESP_LOGE(TAG_ASSiGN, "ID %d is existed", device_id);
+                    device_id++;
+                }
+                else
+                {
+                    ESP_LOGI(TAG_ASSiGN, "Assign successull!--Device ID: %d", device_id);
+                    for (int i = 0; i < 2; i++)
+                    {
+                        led_toggle();
+                        vTaskDelay(LED_DELAY / portTICK_PERIOD_MS);
+                    }
+                }
             }
             else
             {
@@ -192,25 +185,7 @@ void http_get_handle()
     }
 
     cleanup_and_exit(client, url, output_buffer);
-}
-
-void oneshot_adc_read(int *value)
-{
-    adc_oneshot_unit_handle_t adc1_handle;
-    adc_oneshot_unit_init_cfg_t init_config1 = {
-        .unit_id = ADC_UNIT_1,
-    };
-    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
-
-    adc_oneshot_chan_cfg_t config = {
-        .atten = ADC_ATTEN_DB_11,
-        .bitwidth = ADC_BITWIDTH_12,
-    };
-    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, EXAMPLE_ADC1_CHAN0, &config));
-    ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, EXAMPLE_ADC1_CHAN0, value));
-    ESP_LOGI(TAG_ADC, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN0, *value);
-
-    ESP_ERROR_CHECK(adc_oneshot_del_unit(adc1_handle));
+    return is_existed;
 }
 
 void http_post_data(void *pvParameters)
@@ -230,7 +205,7 @@ void http_post_data(void *pvParameters)
                 ESP_LOGE(TAG_HTTP_POST, "Failed to allocate memory for post_data");
                 vTaskDelete(NULL);
             }
-            snprintf(post_data, MAX_POST_SIZE + 40, "{\"device_id\":\"%d\",\"data\":\"%s\"}", DEVICE_ID, temp[tempPostIndex]);
+            snprintf(post_data, MAX_POST_SIZE + 40, "{\"device_id\":\"%d\",\"data\":\"%s\"}", device_id, temp[tempPostIndex]);
             if (http_post_json_handle(post_data) == 0)
             {
                 writeStage = 0;
@@ -281,10 +256,10 @@ void adc_oneshot_write(void *pvParameters)
 
     while (1)
     {
+        uint32_t current_time_milliseconds = esp_log_timestamp();
         int value;
         oneshot_adc_read(&value);
 
-        uint32_t current_time_milliseconds = esp_log_timestamp();
         sprintf(temp[tempWriteIndex] + strlen(temp[tempWriteIndex]), "?%ld&%d", current_time_milliseconds, value);
 
         count++;
@@ -306,6 +281,143 @@ void adc_oneshot_write(void *pvParameters)
         vTaskDelayUntil(&xLastWakeTime, xFrequency);
     }
 }
+
+void cleanup_and_exit(esp_http_client_handle_t client, char *url, char *output_buffer)
+{
+    if (client)
+    {
+        esp_http_client_cleanup(client);
+    }
+    if (url)
+    {
+        vPortFree(url);
+    }
+    if (output_buffer)
+    {
+        vPortFree(output_buffer);
+    }
+}
+
+int http_post_json_handle(char *post_data)
+{
+    esp_http_client_handle_t client = esp_http_client_init(&(esp_http_client_config_t){
+        .url = "http://192.168.58.79:8888/esp",
+        .event_handler = _http_event_handler,
+        .method = HTTP_METHOD_POST,
+    });
+
+    if (!client)
+    {
+        ESP_LOGE(TAG_HTTP_POST, "Failed to initialize HTTP client");
+        return 1;
+    }
+
+    esp_http_client_set_header(client, "Content-Type", "application/json");
+    esp_http_client_set_post_field(client, post_data, strlen(post_data));
+
+    esp_err_t err = esp_http_client_perform(client);
+    if (err == ESP_OK)
+    {
+        ESP_LOGI(TAG_HTTP_POST, "HTTP POST Status = %d, content_length = %lld",
+                 esp_http_client_get_status_code(client),
+                 esp_http_client_get_content_length(client));
+    }
+    else
+    {
+        ESP_LOGE(TAG_HTTP_POST, "HTTP POST request failed: %s", esp_err_to_name(err));
+    }
+
+    cleanup_and_exit(client, NULL, NULL);
+    return (err == ESP_OK) ? 0 : 1;
+}
+
+void http_get_handle()
+{
+    char *url = (char *)pvPortMalloc(40);
+    if (!url)
+    {
+        ESP_LOGE(TAG_HTTP_GET, "Failed to allocate memory for URL");
+        return;
+    }
+
+    char *output_buffer = (char *)pvPortMalloc(MAX_HTTP_OUTPUT_BUFFER + 1);
+    if (!output_buffer)
+    {
+        ESP_LOGE(TAG_HTTP_GET, "Failed to allocate memory for output buffer");
+        vPortFree(url);
+        return;
+    }
+
+    snprintf(url, 40, "%s/device?id=%d", HOST, device_id);
+
+    esp_http_client_config_t config = {
+        .url = url,
+        .method = HTTP_METHOD_GET,
+    };
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+
+    if (!client)
+    {
+        ESP_LOGE(TAG_HTTP_GET, "Failed to initialize HTTP client");
+        cleanup_and_exit(NULL, url, output_buffer);
+        return;
+    }
+
+    esp_err_t err = esp_http_client_open(client, 0);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG_HTTP_GET, "Failed to open HTTP connection: %s", esp_err_to_name(err));
+    }
+    else
+    {
+        int content_length = esp_http_client_fetch_headers(client);
+        if (content_length < 0)
+        {
+            ESP_LOGE(TAG_HTTP_GET, "HTTP client fetch headers failed");
+        }
+        else
+        {
+            int data_read = esp_http_client_read_response(client, output_buffer, MAX_HTTP_OUTPUT_BUFFER);
+            if (data_read >= 0)
+            {
+                ESP_LOGI(TAG_HTTP_GET, "HTTP GET Status = %d, content_length = %" PRId64,
+                         esp_http_client_get_status_code(client),
+                         esp_http_client_get_content_length(client));
+                if (esp_http_client_get_status_code(client) == 404)
+                {
+                    devide_id_assign();
+                }
+                logs = output_buffer[23] - '0';
+            }
+            else
+            {
+                ESP_LOGE(TAG_HTTP_GET, "Failed to read response");
+            }
+        }
+    }
+
+    cleanup_and_exit(client, url, output_buffer);
+}
+
+void oneshot_adc_read(int *value)
+{
+    adc_oneshot_unit_handle_t adc1_handle;
+    adc_oneshot_unit_init_cfg_t init_config1 = {
+        .unit_id = ADC_UNIT_1,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
+
+    adc_oneshot_chan_cfg_t config = {
+        .atten = ADC_ATTEN_DB_11,
+        .bitwidth = ADC_BITWIDTH_12,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, EXAMPLE_ADC1_CHAN0, &config));
+    ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, EXAMPLE_ADC1_CHAN0, value));
+    ESP_LOGI(TAG_ADC, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN0, *value);
+
+    ESP_ERROR_CHECK(adc_oneshot_del_unit(adc1_handle));
+}
+
 void led_toggle()
 {
     ESP_LOGI(TAG_HTTP_POST, "Toggling LED...");
@@ -315,25 +427,4 @@ void led_toggle()
     // Tắt đèn LED
     gpio_set_level(LED_PIN, 0);
     // vTaskDelay(LED_DELAY / portTICK_PERIOD_MS);
-}
-void app_main(void)
-{
-    ESP_ERROR_CHECK(nvs_flash_init());
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    ESP_ERROR_CHECK(example_connect());
-
-    // Configure GPIO for LED
-    gpio_config_t io_conf = {
-        .pin_bit_mask = (1ULL << LED_PIN),
-        .mode = GPIO_MODE_OUTPUT,
-    };
-    gpio_config(&io_conf);
-
-    xTaskCreatePinnedToCore(http_get_data, "Task0", 8192, NULL, 0, &task_http_get_data, 1);
-    xTaskCreatePinnedToCore(http_post_data, "Task1", 8192, NULL, 1, &task_http_post_data, 1);
-    xTaskCreatePinnedToCore(adc_oneshot_write, "Task2", 8192, NULL, 1, &task_adc_oneshot_write, 0);
-
-    vTaskSuspend(task_http_post_data);
-    vTaskSuspend(task_adc_oneshot_write);
 }
